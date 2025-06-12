3b2e4fe0a2f699e53c0b6fe6d6879b9a3cd2e181
Duncan <duncan.adams@stonybrook.edu>
2025-06-12 09:24:00 -0400

    Implemented import_from_bibfile and added test case

--- src/refman/test/test_db.py
+++ src/refman/test/test_db.py
@@ -11,6 +11,10 @@ from refman.ref.bib_entry import BibEntry, iter_entries_from_file
 test_title = "Creative Title"
 test_author = "Anne Author"
 
+test_bibfile = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'adams.bib'
+)
+
 class TestDatabase:
     def test_add_bib_entry(self):
         ref_db = rdb.RefDB(":memory:")
@@ -55,11 +59,26 @@ class TestDatabase:
         ent.author = test_author
         ref_db.add_bib_entry(ent)
         
-        ents = ref_db.search_in_author(test_author)
-        assert ent.title == test_title 
-        assert ent.author == test_author
+        search_result = ref_db.search_in_author(test_author)
+        ent_searched = next(search_result, None)
         
-        ents = ref_db.search_in_author("stupid head")
+        assert (ent_searched is not None)
+        assert ent_searched.title == test_title 
+        assert ent_searched.author == test_author
+        
+        #search for an author that doesnt exists, should return None when we try to get the result
+        search_result = ref_db.search_in_author("stupid head")
+        ent_searched = next(search_result, None)
 
-        assert (list(ents) == [] )
-
+        assert (ent_searched is None)
+        
+    def test_import_from_bibfile(self):
+        ref_db = rdb.RefDB(":memory:")
+        result = ref_db.import_from_bibfile(test_bibfile)
+        
+        assert all(result)
+        
+        search_result = ref_db.search_in_author("Adams")
+        ent_searched = next(search_result, None)
+        
+        assert ent_searched.author == "Adams, Duncan and Baxter, Daniel and Day, Hannah and Essig, Rouven and Kahn, Yonatan"
